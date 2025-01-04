1548f9f665d1e3de53404d5daf8f4bf1a7402ab5
Bill-Nova <BillRan@Protonmail.com>
2025-01-05 06:27:20 +0900

    Update C Code.c

--- C Code.c
+++ C Code.c
@@ -24,7 +24,7 @@ int main(){
 	}
 	int* Sort_Add=(int*)malloc((Input_Max*2)*sizeof(int));	//!!!And If you want this code to run really fast, replace malloc() with a REAL Add[]!!!
 	if(Sort_Add==NULL){ 					//!!!				Sort Add Max=Input Add Max*2			     !!!
-		printf("ERR!!!");
+		printf("ERR!!!");				//I didn't want to write this but considering the average level I saw on reddit before.....
 		goto End;
 	}
 	
